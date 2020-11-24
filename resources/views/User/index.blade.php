df5ff5a1d4121758ade554fb5415a2ef3f0ae265
ophelie <ophelie.manca@gmails.com>
2020-11-24 11:07:23 +0100

    added index in user controller; added view index

--- resources/views/User/index.blade.php
+++ resources/views/User/index.blade.php
@@ -1,1 +1,7 @@
 <!-- affiche tous les utilisateurs -->
+{{-- pour chaque ligne affiche un utilisateurs --}}
+
+@foreach ($users as $user)
+    <a href="{{ route('User.show', ['User' => $user]) }} ">
+      {{ $user->email }}</a>
+@endforeach
