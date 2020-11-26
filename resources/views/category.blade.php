f145423ae13537bcd42d36c8961535e10c50cd35
FrancisClerfayt <francis.clerfayt@gmail.com>
2020-11-26 14:24:00 +0100

    added view for all categories

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -1,25 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="preconnect" href="https://fonts.gstatic.com"> 
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="{{ asset('css/category.css') }}">
-  <title>Category</title>
-</head>
-<body>
-  <section>
-    <h1 class="title-category">Toutes les catégories</h1>
+@extends('layouts.app')
 
-    <div class="content-all-category">
-      @foreach ($categories as $category)
-      <div class="rectangle">
-      <p class="name-category">{{ $category->name }}</p>
-      </div>
-      @endforeach
-    </div>
-  </section>
-</body>
-</html>
+@section('title', 'Toutes les catégories')
+
+@section('content')
+<div class="container">
+	<div class="row">
+		<h2 class="mx-2 ">Toutes les catégories</h2>
+	</div>
+	<div class="row">
+		<div class="col"></div>
+		<div class="col-10">
+			<div class="row my-2 justify-content-around align-items-center">
+				@foreach ($categories as $category)
+				<div class="d-flex align-items-center justify-content-center col-3 px-2 m-2 category_rectangle">
+					<p class="m-0 text-center category_name"> {{ $category->name }} </p>
+				</div>
+				@endforeach
+			</div>
+		</div>
+		<div class="col"></div>
+	</div>
+</div>
+@endsection
